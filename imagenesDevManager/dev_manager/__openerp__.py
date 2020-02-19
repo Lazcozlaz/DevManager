b0ed6a7e3181c8146b2292d855f9dbf14157e257
Lazcozlaz <50100130+Lazcozlaz@users.noreply.github.com>
2020-02-19 18:45:16 +0100

    Add files via upload

---
+++ imagenesDevManager/dev_manager/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Developments Manager',
+    'version' : '1.1',
+    'author' : 'EEP - Profesor',
+    'description' : """ Modulo gestion de solicitudes de desarrollo""",
+    'depends' : ['base'],
+    'application' : True,
+    'data' : ['views/devman_views.xml',
+                'dev_manager_report.xml',
+                'views/report_devman_task_hours.xml',
+                'views/report_devman_task_responsible.xml',
+                ],
+}
